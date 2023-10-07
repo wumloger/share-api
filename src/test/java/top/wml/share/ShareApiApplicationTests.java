73a81589b53945afccecfc3db2d941bf801d5163
ray <656482519@qq.com>
2023-10-07 10:11:40 +0800

    初始化项目

---
+++ src/test/java/top/wml/share/ShareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package top.wml.share;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
