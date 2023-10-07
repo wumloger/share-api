3dda25525c959cfa28415b74d4813c8ca119f768
ray <656482519@qq.com>
2023-10-07 10:44:19 +0800

    修改启动日志，增加 /user-service 上下文，配置 logback 日志

--- share-user/src/main/java/top/wml/share/user/config/UserApplication.java
+++ share-user/src/main/java/top/wml/share/user/config/UserApplication.java
@@ -1,13 +1,19 @@
 package top.wml.share.user.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 @ComponentScan("top.wml")
+@Slf4j
 public class UserApplication {
     public static void main(String[] args) {
-        SpringApplication.run(UserApplication.class,args);
+        SpringApplication app = new SpringApplication(UserApplication.class);
+        ConfigurableEnvironment env = app.run(args).getEnvironment();
+        log.info("启动成功！");
+        log.info("测试地址：http://127.0.0.1:{}{}/hello",env.getProperty("server.port"),env.getProperty("server.servlet.context-path"));
     }
 }
