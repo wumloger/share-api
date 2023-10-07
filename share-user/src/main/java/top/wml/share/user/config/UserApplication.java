e414df169b73e9d62092fcc4150ef0bd7c09ec9f
ray <656482519@qq.com>
2023-10-07 15:03:43 +0800

    增加 LogAspect，打印请求参数和返回结果及日志流水号

--- share-user/src/main/java/top/wml/share/user/config/UserApplication.java
+++ share-user/src/main/java/top/wml/share/user/config/UserApplication.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
